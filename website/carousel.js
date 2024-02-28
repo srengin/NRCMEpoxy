d227b65c06ccaef6973128726d42a29f0849e186
Susan Engin <susan.engin@gmail.com>
2024-02-28 17:40:39 -0500

    StartOver

---
+++ website/carousel.js
@@ -0,0 +1,80 @@
+carousel = document.getElementById("carousel1");
+carousel.innerHTML = `<div id="carouselExample" class="carousel slide">
+<div class="carousel-inner">
+  <div class="carousel-item active"  style="background-image: url('images/EpoxyGarage.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;">
+    <img src="..." class="d-block w-100" alt="...">
+    NRCM Epoxy: Veteran and Family Owned
+  </div>
+  <div class="carousel-item" style="background-image: url('images/epoxyroll.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;">
+    <img src="..." class="d-block w-100" alt="...">
+    What is Epoxy Flooring?
+  </div>
+  <div class="carousel-item" style="background-image: url('images/garage.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;">
+    <img src="..." class="d-block w-100" alt="...">
+    Why should you buy from us?
+  </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div>`
+
+
+
+
+/*
+`<div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" >
+        <div style="background=red">
+        <!--<div style="background-image: url('images/EpoxyGarage.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;"> -->
+        <!--<img src="images/EpoxyGarage.jpeg"  width="100%" height="auto" background="var(--bs-secondary-color)" text="false" title="false" > -->
+          <div class="container" >
+            <div class="carousel-caption text-start">
+            <h1>Example headline.</h1>
+            <p class="opacity-75">Some representative placeholder content fornmbm,nb,mnb the first slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item" style="background-image: url('images/epoxyroll.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;"></div>>
+        <!--<img src="images/EpoxyGarage.jpeg"width="100%" background="var(--bs-secondary-color)" text="false" title="false" >-->
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Another example headline.......</h1>
+            <p>Some representative placeholder content for the second slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item" style="background-image: url('images/garage.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;"></div>>
+        <!-- <img src="images/EpoxyGarage.jpeg" width="100%" height="100%" background="var(--bs-secondary-color)" text="false" title="false" > -->
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <h1>One more for good measure.</h1>
+            <p>Some representative placeholder content for the third slide of this carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+    </div>`; */
---
+++ website/marketing.js
@@ -0,0 +1,29 @@
+/*<!-- Marketing messaging and featurettes
+  ================================================== -->
+  <!-- Wrap the rest of the page in another container to center all the content. -->
+
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class="col-lg-4">
+        <img src="images/EpoxyGarage.jpeg" width="140" height="140" background="var(--bs-secondary-color)" text="false" class="rounded-circle" >
+        <h2 class="fw-normal">Heading</h2>
+        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="images/EpoxyGarage.jpeg" width="140" height="140" background="var(--bs-secondary-color)" text="false" class="rounded-circle" >
+        <h2 class="fw-normal">Heading</h2>
+        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="images/EpoxyGarage.jpeg" width="140" height="140" background="var(--bs-secondary-color)" text="false" class="rounded-circle" >
+        <h2 class="fw-normal">Heading</h2>
+        <p>And lastly this, the third column of representative placeholder content.</p>
+        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+
+*/
