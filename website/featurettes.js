16c0ce87a3ef185ebe32ff369c9b03c605216423
Susan Engin <susan.engin@gmail.com>
2024-02-29 09:29:43 -0500

    Features

--- website/featurettes.js
+++ website/featurettes.js
@@ -2,8 +2,17 @@ let featurettes = document.getElementById("featurettes");
 featurettes.innerHTML =`<!--<hr class="featurette-divider">-->
     <div class="row featurette" >
       <div class="col-md-7">
-        <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-body-secondary">It’ll blow your mind.</span></h2>
-        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+        <h2 class="featurette-heading fw-normal lh-1">Here at NRCM Epoxy we are transforming your concrete with <span class="text-body-secondary">surface designs for your lifestyle</span></h2>
+        <p class="lead">It's not just about looks though. Epoxy floors are premium flooring because they durable and resistant to:
+              <ol class="lead">
+                <li>scratches</li>
+                <li>stains</li>
+                <li>chemicals</li>
+                <li>oils</li>
+              </ol>
+        </p>
+        <p class="lead">This makes them the perfect solution to high trafic areas.</p>
+
       </div>
       <div class="col-md-5">
         <img src="images/beforeAndAfter.jpg" width="500"  background="var(--bs-secondary-bg)" color="var(--bs-secondary-color)" class="bd-placeholder-img-lg featurette-image img-fluid mx-auto" >
@@ -14,8 +23,10 @@ featurettes.innerHTML =`<!--<hr class="featurette-divider">-->
 
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        <h2 class="featurette-heading fw-normal lh-1">It's all about the Surface Prep! <span class="text-body-secondary">Acid etching vs. Grinding with Diamond Segments:</span></h2>
+        <p class="lead">Don't get lured in with cheap installation cost, chances are they are doing what is called acid etching. Acid etching is a shortcut but you will get what you pay for.
+          Typically epoxy flooring that has been prepared with acid etching needs to be redone after 1-5 years compared to proper preparation with concrete grinding that can last more than 20 years. </p>
+          <p class="lead">So in the long run you are saving much more time and money by getting it done right the first time.</p>
       </div>
       <div class="col-md-5 order-md-1">
         <img src="images/image15.jpeg" width="500" height="500" background="var(--bs-secondary-bg)" color="var(--bs-secondary-color)" class="bd-placeholder-img-lg featurette-image img-fluid mx-auto" >
