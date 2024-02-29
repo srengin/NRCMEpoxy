ec2c55c34a761d466b0349b2133c1b66e07dd4eb
Susan Engin <susan.engin@gmail.com>
2024-02-28 22:13:58 -0500

    links

--- website/navbar.js
+++ website/navbar.js
@@ -1,20 +1,20 @@
 navbar = document.getElementsByTagName("header");
 navbar[0].innerHTML = `<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand"  display="inline-block" height="1rem" href="#"><img src="images/whitelogo.png"  display="inline-block"  style="height:3rem;"></a>
+      <a class="navbar-brand"  display="inline-block" height="1rem" href="index.html"><img src="images/whitelogo.png"  display="inline-block"  style="height:3rem;"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Getting Started</a>
+            <a class="nav-link" href="getstarted.html">Getting Started</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="aboutus.html">About Us</a>
           </li>
         </ul>
       </div>
