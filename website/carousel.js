f8074ab22ecf6afa610253c2d7ac29f254407d1f
Susan Engin <susan.engin@gmail.com>
2024-02-28 19:28:36 -0500

    Carousel2

--- website/carousel.js
+++ website/carousel.js
@@ -1,5 +1,56 @@
-carousel = document.getElementById("carousel1");
-carousel.innerHTML = `<div id="myCarousel" class="carousel slide">
+carousel = document.getElementById("carousel");
+carousel.innerHTML = `<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+<div class="carousel-indicators">
+  <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+</div>
+<div class="carousel-inner">
+  <div class="carousel-item active">
+  <img src="images/EpoxyGarage.jpeg" class="d-block w-100" alt="Epoxy Garage">
+    <!--{{< placeholder width="100%" height="100%" background="#777" color="#777" text="false" title="false" >}}-->
+    <div class="container">
+      <div class="carousel-caption text-start">
+        <h1>Example headline.</h1>
+        <p>Some representative placeholder content for the first slide of the carousel.</p>
+        <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="carousel-item">
+  <img src="images/epoxyroll.jpeg" class="d-block w-100" alt="Epoxy Garage">
+    <!--{{< placeholder width="100%" height="100%" background="#777" color="#777" text="false" title="false" >}}-->
+    <div class="container">
+      <div class="carousel-caption">
+        <h1>Another example headline.</h1>
+        <p>Some representative placeholder content for the second slide of the carousel.</p>
+        <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="carousel-item">
+  <img src="images/garage.jpeg" class="d-block w-100" alt="Epoxy Garage">
+    <!--{{< placeholder width="100%" height="100%" background="#777" color="#777" text="false" title="false" >}}-->
+    <div class="container">
+      <div class="carousel-caption text-end">
+        <h1>One more for good measure.</h1>
+        <p>Some representative placeholder content for the third slide of this carousel.</p>
+        <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div>`;
+
+/*`<div id="myCarousel" class="carousel slide">
 <div class="carousel-indicators">
     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -12,14 +63,15 @@ carousel.innerHTML = `<div id="myCarousel" class="carousel slide">
 </div>
   </div>
   <div class="carousel-item" style="background-image: url('images/epoxyroll.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;">
-    <img src="..." class="d-block w-100" alt="...">
+    <img src="'images/epoxyroll.jpeg'" class="d-block w-100" alt="...">
     <div class="carousel-caption d-none d-md-block">
     <h2 style="color:black; font-weight:bold; text-shadow: 2px 2px gray; font-family: Copperplate, Papyrus, fantasy; background-color: rgba(104, 99, 71, .1);">Did you know?</h5>
-  <h2 style="color:black; font-weight:bold; text-shadow: 2px 2px gray; font-family: Copperplate, Papyrus, fantasy; background-color: rgba(104, 99, 71, .1);">We use Polyaspartic</h5>
+  <h2 style="color:black; font-weight:bold; text-shadow: 2px 2px gray; font-family: Copperplate, Papyrus, fantasy; background-color: rgba(104, 99, 71, .1);">We use a Polyaspartic Coating which is 
+  it cures faster and can be used in a wide range of temperatures. They are versatile and durable.</h5>
   </div>
-  <div class="carousel-item" style="background-image: url('images/garage.jpeg'); background-size: 100% auto; background-position-y: 40%; background-repeat:no-repeat; background-color: #EFECEC;">
-    <img src="..." class="d-block w-100" alt="...">
-    Why should you buy from us?
+  <div class="carousel-item" >
+  <img src="images/garage.jpeg" class="d-block w-100" alt="..."> 
+  Why should you buy from us?
   </div>
 </div>
 <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
@@ -30,7 +82,7 @@ carousel.innerHTML = `<div id="myCarousel" class="carousel slide">
   <span class="carousel-control-next-icon" aria-hidden="true"></span>
   <span class="visually-hidden">Next</span>
 </button>
-</div>`
+</div>`*/
 
 
 
